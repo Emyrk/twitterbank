e828434026dd384caa4a683da3289ea646640404
Emyrk <stevenmasley@gmail.com>
2019-06-30 09:53:18 -0500

    Switch to count

--- database/insert.go
+++ database/insert.go
@@ -22,16 +22,16 @@ func (db *TwitterBankDatabase) InsertNewUserChain(user *TwitterUser) error {
 
 func (db *TwitterBankDatabase) InsertNewTweet(tweet *TwitterTweetObject, record *TwitterTweetRecord) error {
 	// First check if the tweet exists. If it does, we only need to add another record of it's existence
-	var exists bool
+	var count int
 	d := db.DB.Raw(`
-		SELECT exists(SELECT * FROM twitter_tweet_objects WHERE tweet_hash = '$1' AND tweet_id_str = '$2')
-	`, tweet.TweetHash, tweet.TweetIDStr).First(&exists)
+		SELECT * FROM twitter_tweet_objects WHERE tweet_hash = '$1' AND tweet_id_str = '$2'
+	`, tweet.TweetHash, tweet.TweetIDStr).Count(&count)
 	if d.Error != nil {
 		return fmt.Errorf("exists_query: %s", d.Error.Error())
 	}
 
 	tx := db.DB.Begin()
-	if !exists {
+	if count == 0 {
 		// Insert Tweet first
 		if dbc := db.DB.Create(tweet); dbc.Error != nil {
 			tx.Rollback()
